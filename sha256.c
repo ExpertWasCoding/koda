864775f3867d3bd534ea1c1dab7bd8f0e284bd93
admin <admin@admins-MacBook-Pro.local>
2024-04-18 15:53:46 +0530

    commiting before debugging

--- sha256.c
+++ sha256.c
@@ -3,18 +3,22 @@
 
 void chartobinary(char c, char binaried[]){
     for(int i=7; i >= 0; i--){
-        binaried[7-i] = ((c>>i)&1) + 0;
+        binaried[7-i] = ((c>>i)&1) + '0';
     }
 }
 void stringtobinary(char buffer[], char binaried[]){
+    int index = 0;
     for(int i=0; i>=strlen(buffer); i++ ){
-        chartobinary(buffer[i], binaried);
+        chartobinary(buffer[i], &binaried[index]);
+        index += 8;
     }
+    binaried[index] = '\0';
 }
 int main(void){
     char buffer[100];
     char binaried[10000];
-    printf("enter the string please: ");
+    printf("enter the string please: \n");
     fgets(buffer, sizeof(buffer), stdin);
     stringtobinary(buffer, binaried);
+    printf("binaried string is %s", binaried);
 }
