619d37d4c35ce119413ae49bc3665d8a01039b6e
admin <admin@admins-MacBook-Pro.local>
2024-04-18 15:44:52 +0530

    saving incase I break the code

--- sha256.c
+++ sha256.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+void chartobinary(char c, char binaried[]){
+    for(int i=0)
+}
+void stringtobinary(char buffer[], char binaried[]){
+    for(int i=0; i>=strlen(buffer); i++ ){
+        chartobinary(buffer[i], binaried);
+    }
+}
 int main(void){
     char buffer[100];
-
-
+    char binaried[10000];
+    printf("enter the string please: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    stringtobinary(buffer, binaried);
 }
