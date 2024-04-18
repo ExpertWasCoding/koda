0fd8733a3bf7e7bcbcfcc51830ca1b160206cf44
admin <admin@admins-MacBook-Pro.local>
2024-04-18 16:00:41 +0530

    somewhat moving forward

--- sha256.c
+++ sha256.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-void chartobinary(char c, char binaried[]){
-    for(int i=7; i >= 0; i--){
-        binaried[7-i] = ((c>>i)&1) + '0';
+void chartobinary(char c, char binaried[]) {
+    for (int i = 7; i >= 0; i--) {
+        binaried[7 - i] = ((c >> i) & 1) + '0';
     }
 }
-void stringtobinary(char buffer[], char binaried[]){
+
+void stringtobinary(char buffer[], char binaried[]) {
+    int len = strlen(buffer);
     int index = 0;
-    for(int i=0; i>=strlen(buffer); i++ ){
+    for (int i = 0; i < len; i++) {
         chartobinary(buffer[i], &binaried[index]);
         index += 8;
     }
     binaried[index] = '\0';
 }
-int main(void){
+
+int main(void) {
     char buffer[100];
     char binaried[10000];
-    printf("enter the string please: \n");
+    printf("Enter the string: ");
     fgets(buffer, sizeof(buffer), stdin);
     stringtobinary(buffer, binaried);
-    printf("binaried string is %s", binaried);
+
+    printf("Binary representation: %s\n", binaried);
+
+    return 0;
 }
